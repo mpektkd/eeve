09485e979dcf948dd3bc6242f108a588a4c748a2
adonistseriotis <adonis.tseriotis@gmail.com>
2021-02-01 20:13:53 +0200

    Added validators

--- Backend/eevie/models.py
+++ Backend/eevie/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.fields import CharField
+
+from . import validators
 
  # Customer
 class Customer(models.Model):
@@ -21,7 +24,7 @@ class Bill(models.Model):
 
  # Monthly Bill expires every 1st of month
 class MonthlyBill(models.Model):
-    customer = models.ManyToManyField(User, related_name="monthlybills")
+    customer = models.ForeignKey(User, related_name="monthlybills", on_delete=models.CASCADE) # Many to One relationship with Customers
     monthly_total = models.FloatField()
     start_date = models.DateField()
     end_date = models.DateField()
@@ -31,7 +34,7 @@ class MonthlyBill(models.Model):
 
  # Stored cards for customer
 class Card(models.Model):
-    customer = models.ForeignKey(User, related_name="cards", on_delete=models.CASCADE)
+    customer = models.ForeignKey(User, related_name="cards", on_delete=models.CASCADE) # Many to One relationship with Customers
     card_no = models.IntegerField()
 
     def __str__(self):
@@ -39,15 +42,55 @@ class Card(models.Model):
         last_dig = self.card_no % 10000
         return f"{first_dig}********{last_dig}"
 
+
+class Ports(models.Model):
+    id = models.IntegerField()
+    title = models.CharField()
+
+    def __str__(self):
+        return f"Port with ID: {self.id} and title {self.title}."
+
+class Brands(models.Model):
+    id = models.CharField()
+    name = models.CharField()
+
+    def __str__(self):
+        return f"Brand {self.name} has ID: {self.id}."
+
+class ACcharger(models.Model):
+    usable_phases = models.IntegerField()
+    ports = models.ManyToManyField(Ports, on_delete=models.DO_NOTHING) # Many to Many relationship to existing Ports
+    max_power = models.FloatField()
+    # Might want to insert power_per_charging_point afterwards
+
+    def __str__(self):
+        return f"AC charger with {self.usable_phases} phases, available ports: {self.ports} and {self.max_power} max power."
+
+class chargingCurve(models.Model):
+    percentage = models.FloatField(validators=[validators.validate_percentage])
+    power = models.FloatField()
+
+    def __str__(self):
+        return f"Charging percentage {self.percentage} with power {self.power}."
+
+class DCcharger(models.Model):
+    ports = models.ManyToManyField(Ports, on_delete=models.DO_NOTHING) # Many to Many relationship to existing Ports
+    max_power = models.FloatField()
+    charging_curve = models.ManyToManyField(chargingCurve, on_delete=models.DO_NOTHING)
+
+    def __str__(self):
+        return f"DC charger with available ports: {self.ports}."
+
 class Car(models.Model):
-    CAR_TYPE = (
-        ('BEV', 'Battery Electric Vehicle'),
-        ('PHEV', 'Plug-in Hybrid Electric Vehicle'),
-        ('HEV', 'Hybrid Electric Vehicle')
-    )
-    customer = models.ForeignKey(User, related_name="cars", on_delete=models.CASCADE)
+    id = models.CharField()
+    brand = models.OneToOneField(Brands, on_delete=models.DO_NOTHING)
     model = models.CharField()
-    car_type = models.CharField(max_length=4, choices=CAR_TYPE)
+    release_year = models.IntegerField(validators=[validators.validate_year])
+    usable_battery_size = models.FloatField(validators=[validators.validate_percentage])
+    ac_charger = models.OneToOneField(ACcharger, on_delete=models.DO_NOTHING)
+    dc_charger = models.OneToOneField(DCcharger, on_delete=models.DO_NOTHING)
+    average_consumption = models.FloatField(validators=[validators.validate_percentage])
+    customer = models.ForeignKey(User, related_name="cars", on_delete=models.CASCADE)
 
     def __str__(self):
         return f"Car with ID: {self.id},model {self.model} and type {self.get_car_type_display()} belongs to {self.customer.get_username()}."
