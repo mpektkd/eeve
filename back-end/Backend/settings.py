12ed77cddf058bca4ff9c17bdcfdf62a8fd90ba1
adonistseriotis <adonis.tseriotis@gmail.com>
2021-03-14 19:27:22 +0200

    Added simple jwt again

--- back-end/Backend/settings.py
+++ back-end/Backend/settings.py
@@ -150,6 +150,7 @@ REST_FRAMEWORK = {
 
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'eevie.authentication.SafeJWTAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication'
     ),
 }
 
