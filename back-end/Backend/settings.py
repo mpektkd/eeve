90f0da2124a040de5bdbb2242822b409768edf83
adonistseriotis <adonis.tseriotis@gmail.com>
2021-03-14 19:23:11 +0200

    Test

--- back-end/Backend/settings.py
+++ back-end/Backend/settings.py
@@ -168,17 +168,17 @@ SIMPLE_JWT = {
     'TOKEN_TYPE_CLAIM': 'token_type',
 }
 
-# CORS_ORIGIN_WHITELIST = (
-#     'http://localhost:3000',
-#     'http://0.0.0.0:3000',
-#     'http://192.168.1.9:3000',
-#     'http://192.168.1.16:3000',
-#     'http://192.168.1.2:3000',
-#     'http://192.168.1.20:3000',
-#     'http://172.20.10.2:3000'
-# )
+CORS_ORIGIN_WHITELIST = (
+    'http://localhost:3000',
+    'http://0.0.0.0:3000',
+    'http://192.168.1.9:3000',
+    'http://192.168.1.16:3000',
+    'http://192.168.1.2:3000',
+    'http://192.168.1.20:3000',
+    'http://172.20.10.2:3000'
+)
 
-CORS_ORIGIN_ALLOW_ALL=True
+# CORS_ORIGIN_ALLOW_ALL=True
 CORS_ALLOW_CREDENTIALS=False
 
 CORS_ALLOW_HEADERS=[
