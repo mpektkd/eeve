cdb70a4adf7270a715a7aaa97e888017e5d5487f
adonistseriotis <adonis.tseriotis@gmail.com>
2021-03-14 18:15:10 +0200

    Custom auth header fix

--- back-end/Backend/settings.py
+++ back-end/Backend/settings.py
@@ -162,7 +162,7 @@ SIMPLE_JWT = {
     'SIGNING_KEY': SECRET_KEY,
     'VERIFYING_KEY': None,
     'AUTH_HEADER_TYPES': ('JWT'),
-    'AUTH_HEADER_NAME' : 'HTTP_X_OBSERVATORY_AUTH'
+    'AUTH_HEADER_NAME' : HTTP_X_OBSERVATORY_AUTH,
     'USER_ID_FIELD': 'id',
     'USER_ID_CLAIM': 'user_id',
     'AUTH_TOKEN_CLASSES': ('rest_framework_simplejwt.tokens.AccessToken',),
